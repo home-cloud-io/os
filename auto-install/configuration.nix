953bd78b1caa65d9b01cd7c514d06ee4e0cb90f3
Jack Kawell <jack@kawell.us>
2024-11-07 09:17:44 -0600

    initial pass at configuration.nix

--- auto-install/configuration.nix
+++ auto-install/configuration.nix
@@ -13,6 +13,55 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.bcache.enable = false;
 
+  networking.nat.enable = true;
+  networking.nat.externalInterface = "eth0";
+  networking.nat.internalInterfaces = [ "wg0" ];
+
+
+  networking.wireguard.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP address and subnet of the server's end of the tunnel interface.
+      ips = [ "10.100.0.1/24" ];
+
+      # The port that WireGuard listens to. Must be accessible by the client.
+      # TODO: Does this port need to be available externally via STUN? Do we want a unique port per client? Does that require multiple interfaces (one per client)?
+      listenPort = 51820;
+
+      # TODO: Give the user an option to tunnel all their traffic through Home Cloud
+      # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+      # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
+      # postSetup = ''
+      #   ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      # '';
+
+      # This undoes the above command
+      # postShutdown = ''
+      #   ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 10.100.0.0/24 -o eth0 -j MASQUERADE
+      # '';
+
+      # TODO: Generate this file when the user enables the feature.
+      # Path to the private key file.
+      #
+      # Note: The private key can also be included inline via the privateKey option,
+      # but this makes the private key world-readable; thus, using privateKeyFile is
+      # recommended.
+      privateKeyFile = "/etc/home-cloud/wireguard-keys/private";
+
+      # TODO: These keys will be sent to the daemon from the server when adding clients through the admin dashboard.
+      peers = [
+        {
+          publicKey = "qQPgg/fjW+ciCM2AO1NqxCs7kCa+NhMXc3VGK+bYWnE=";
+          allowedIPs = [ "10.100.0.2/32" ];
+        }
+        {
+          publicKey = "gTw4fvPlrASYbEhRXtqnZEK7p6pftrzNE5EHHYxhCEI=";
+          allowedIPs = [ "10.100.0.3/32" ];
+        }
+      ];
+    };
+  };
+
   # TODO: this is either not triggering or is triggering after the daemon has already been killed
   # systemd.services.shutdown-alert = {
   #   enable = true;
@@ -149,6 +198,8 @@ in
       pkgs.nano
       pkgs.openssl
       pkgs.nvd
+      # pkgs.wireguard
+      # pkgs.wireguard-tools
     ];
 
   # This option defines the first version of NixOS you have installed on this particular machine,
