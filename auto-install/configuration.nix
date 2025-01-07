fd66480cbcde9f957faffcd3a5a05a0f43b9b721
Jack Kawell <jack@kawell.us>
2025-01-07 17:24:31 -0600

    move to 24.11 stable

--- auto-install/configuration.nix
+++ auto-install/configuration.nix
@@ -49,6 +49,8 @@ in
       pkgs.nvd
     ];
 
+  system.defaultChannel = "https://channels.nixos.org/nixos-24.11";
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
@@ -66,5 +68,5 @@ in
   # and migrated your data accordingly.
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.11"; # Did you read the comment?
 }
