e3029b4109ded208fda1a37f0184795cea8ce396
Jack Kawell <jack@kawell.us>
2025-04-14 12:52:34 -0500

    daemon -> v0.0.34

--- auto-install/home-cloud/daemon/default.nix
+++ auto-install/home-cloud/daemon/default.nix
@@ -2,8 +2,8 @@ with import <nixpkgs> {};
 
 buildGoModule rec {
   pname = "home-cloud-daemon";
-  version = "v0.0.29";
-  vendorHash = "sha256-krte40y6rGq95UUg8WfVubDOnZUK6f9XsZ5uJA9j/h4=";
+  version = "v0.0.34";
+  vendorHash = "sha256-FqzosyTCmAdKedbRn4yISu1Z7B595f6L+E3qRJit5UA=";
 
   meta = with lib; {
     description = "Home Cloud Host Daemon";
@@ -17,6 +17,6 @@ buildGoModule rec {
     owner = "home-cloud-io";
     repo = "core";
     rev = "services/platform/daemon/${version}";
-    hash = "sha256-nnOSH+qEpO9EYWDqjNfFoAw/sqSlRI3ku6wiNTdLF5A=";
+    hash = "sha256-SjVaMsNQEG7Ds0X+B/1eMVRsOw6Q8WeP9mNWUXwGuuo=";
   } + "/services/platform/daemon";
 }
